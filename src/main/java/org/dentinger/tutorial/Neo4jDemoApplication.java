7077fa4d0d6391feca1a190b9c902812890fe1ca
Dan Dentinger <daniel.dentinger@kroger.com>
2016-04-04 13:51:10 -0400

    Cleaned up some code

--- src/main/java/org/dentinger/tutorial/Neo4jDemoApplication.java
+++ src/main/java/org/dentinger/tutorial/Neo4jDemoApplication.java
@@ -2,15 +2,8 @@ package org.dentinger.tutorial;
 
 import java.util.Arrays;
 import java.util.List;
-import org.dentinger.tutorial.autoconfig.Neo4jProperties;
-import org.dentinger.tutorial.domain.depricated.Person;
 import org.dentinger.tutorial.loader.LeagueLoader;
 import org.dentinger.tutorial.loader.RegionLoader;
-import org.dentinger.tutorial.repository.PersonRepository;
-
-import org.neo4j.ogm.session.Session;
-import org.neo4j.ogm.session.SessionFactory;
-import org.neo4j.ogm.session.transaction.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -41,62 +34,5 @@ public class Neo4jDemoApplication {
 		};
 	}
 
-//	@Bean CommandLineRunner demo(PersonRepository personRepository,
-//                               SessionFactory sessionFactory, Neo4jProperties neo4jProperties) {
-//
-//    Session session = sessionFactory.openSession(neo4jProperties.getUrl(),
-//        neo4jProperties.getUsername(), neo4jProperties.getPassword());
-//
-//		return args -> {
-//
-//			Person greg = new Person("Greg");
-//			Person roy = new Person("Roy");
-//			Person craig = new Person("Craig");
-//
-//			List<Person> team = Arrays.asList(greg, roy, craig);
-//
-//			log.info("Before linking up with Neo4j...");
-//
-//			team.stream()
-//					.forEach(person -> log.info("\t" + person.toString()));
-//
-//			Transaction tx = session.beginTransaction();
-//			try {
-//				personRepository.save(greg);
-//				personRepository.save(roy);
-//				personRepository.save(craig);
-//
-//				greg = personRepository.findByName(greg.getName());
-//				greg.worksWith(roy);
-//				greg.worksWith(craig);
-//				personRepository.save(greg);
-//
-//				roy = personRepository.findByName(roy.getName());
-//				roy.worksWith(craig);
-//				// We already know that roy works with greg
-//				personRepository.save(roy);
-//
-//				// We already know craig works with roy and greg
-//
-//
-//				log.info("Lookup each person by name...");
-//				team.stream()
-//						.forEach(person ->
-//								log.info("\t" + personRepository
-//										.findByName(person.getName()).toString()));
-//
-//
-//				log.info("Lookup each person by teammate...");
-//				for (Person person : team) {
-//					log.info(person.getName() + " is a teammate of...");
-//					personRepository.findByTeammates_Name(person.getName()).stream()
-//							.forEach(teammate -> log.info("\t" + teammate.getName()));
-//				}
-//
-//			//	tx.commit();
-//			} finally {
-////				tx.close();
-//			}
-//		};
-//	}
+
 }
