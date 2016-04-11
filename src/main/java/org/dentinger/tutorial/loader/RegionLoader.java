305612aa336bd95dcf39186e6c35e76988021636
Ron Rittinger <ron.rittinger@kroger.com>
2016-04-11 19:08:49 -0400

    Added multipl threads for region loading

--- src/main/java/org/dentinger/tutorial/loader/RegionLoader.java
+++ src/main/java/org/dentinger/tutorial/loader/RegionLoader.java
@@ -1,8 +1,15 @@
 package org.dentinger.tutorial.loader;
 
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import org.dentinger.tutorial.autoconfig.Neo4jProperties;
 import org.dentinger.tutorial.dal.SportsBallRepository;
 import org.dentinger.tutorial.domain.Region;
@@ -12,6 +19,7 @@ import org.neo4j.ogm.session.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.neo4j.template.Neo4jTemplate;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +29,8 @@ public class RegionLoader {
   private Neo4jProperties neo4jProperties;
   private SessionFactory sessionFactory;
   private SportsBallRepository repo;
+  private int numThreads;
+  private final AtomicLong recordsWritten = new AtomicLong(0);
 
   private String MERGE_REGIONS =
       "unwind {json} as q " +
@@ -29,26 +39,42 @@ public class RegionLoader {
           "  on match set r.name = q.name";
 
   @Autowired
-  public RegionLoader(Neo4jProperties neo4jProperties, SessionFactory sessionFactory, SportsBallRepository repo) {
+  public RegionLoader(Neo4jProperties neo4jProperties, SessionFactory sessionFactory, SportsBallRepository repo, Environment env) {
     this.neo4jProperties = neo4jProperties;
     this.sessionFactory = sessionFactory;
     this.repo = repo;
+    this.numThreads = Integer.valueOf(env.getProperty("regions.loaderThreads","1"));
   }
 
   public void loadRegions() {
-    Neo4jTemplate neo4jTemplate = getNeo4jTemplate();
     AggregateExceptionLogger aeLogger = AggregateExceptionLogger.getLogger(this.getClass());
     List<Region> regions = repo.getRegions();
-    logger.info("About to load {} regions", regions.size());
-    Map<String, Object> map = new HashMap<String, Object>();
-    map.put("json", regions);
+    logger.info("About to load {} regions using {} threads", regions.size(), numThreads);
+    ExecutorService executorService = getExecutorService(numThreads);
+    int subListSize = (int) Math.floor(regions.size() / numThreads);
+
     long start = System.currentTimeMillis();
+    Lists.partition(regions, subListSize).stream().parallel()
+        .forEach((sublist) -> {
+              executorService.submit(() -> {
+                try{
+                  Map<String, Object> map = new HashMap<String, Object>();
+                  map.put("json", sublist);
+                  getNeo4jTemplate().execute(MERGE_REGIONS, map);
+                  logger.info("Processing of sublist (size={}) complete",sublist.size());
+                  recordsWritten.addAndGet(sublist.size());
+                } catch (Exception e) {
+                  aeLogger.error("Unable to update graph, regionCount={}", regions.size(), e);
+                }
+              });
+            });
+    executorService.shutdown();
     try {
-      neo4jTemplate.execute(MERGE_REGIONS, map);
-    } catch (Exception e) {
-      aeLogger.error("Unable to update graph, regionCount={}", regions.size(), e);
+      executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+    }catch(Exception e){
+      logger.error("executorService exception: ",e);
     }
-    logger.info("Processing of {} Regional relationships complete: {}ms", regions.size(), System.currentTimeMillis() - start);
+    logger.info("Processing of {} Regional relationships complete: {}ms", recordsWritten.get(), System.currentTimeMillis() - start);
   }
 
   private Neo4jTemplate getNeo4jTemplate() {
@@ -56,5 +82,13 @@ public class RegionLoader {
         neo4jProperties.getUsername(), neo4jProperties.getPassword());
 
     return new Neo4jTemplate(session);
+  }
+
+  private ExecutorService getExecutorService(int numThreads) {
+    final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+        .setNameFormat("loader-%d")
+        .setDaemon(true)
+        .build();
+    return Executors.newFixedThreadPool(numThreads, threadFactory);
   }
 }
