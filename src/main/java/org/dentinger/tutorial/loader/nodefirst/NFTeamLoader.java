7d9c5ceed236730d4acfc137e51e0d8f367156ee
Dan Dentinger <daniel.dentinger@kroger.com>
2016-05-19 10:38:26 -0400

    updated the team loader to have better partitioning for loading

--- src/main/java/org/dentinger/tutorial/loader/nodefirst/NFTeamLoader.java
+++ src/main/java/org/dentinger/tutorial/loader/nodefirst/NFTeamLoader.java
@@ -1,26 +1,16 @@
 package org.dentinger.tutorial.loader.nodefirst;
 
 import com.google.common.collect.Lists;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 import org.dentinger.tutorial.dal.SportsBallRepository;
-import org.dentinger.tutorial.domain.League;
 import org.dentinger.tutorial.domain.Team;
 import org.dentinger.tutorial.util.AggregateExceptionLogger;
-import org.dentinger.tutorial.util.RetriableTask;
-import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.data.neo4j.template.Neo4jTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +19,7 @@ public class NFTeamLoader {
   private static Logger logger = LoggerFactory.getLogger(NFTeamLoader.class);
   private SessionFactory sessionFactory;
   private SportsBallRepository repo;
+  private TeamWorker submitableWorker;
   private int numThreads;
   private final AtomicLong recordsWritten = new AtomicLong(0);
   private ThreadPoolTaskExecutor poolTaskExecutor;
@@ -52,9 +43,10 @@ public class NFTeamLoader {
   public NFTeamLoader(ThreadPoolTaskExecutor teamProcessorThreadPool,
                       SessionFactory sessionFactory,
                       SportsBallRepository repo,
-                      Environment env) {
+                      Environment env, TeamWorker submitableWorker) {
     this.sessionFactory = sessionFactory;
     this.repo = repo;
+    this.submitableWorker = submitableWorker;
     this.numThreads = Integer.valueOf(env.getProperty("teams.loading.threads", "1"));
     this.poolTaskExecutor = teamProcessorThreadPool;
   }
@@ -70,7 +62,7 @@ public class NFTeamLoader {
     long start = System.currentTimeMillis();
     Lists.partition(teamList, subListSize).stream().parallel()
         .forEach((teams) -> {
-          doSubmitableWork(aeLogger, teams, MERGE_TEAM_NODES);
+          submitableWorker.doSubmitableWork(aeLogger, teams, MERGE_TEAM_NODES);
 
         });
     monitorThreadPool();
@@ -81,21 +73,17 @@ public class NFTeamLoader {
 
   public void loadTeamRelationships() {
     AggregateExceptionLogger aeLogger = AggregateExceptionLogger.getLogger(this.getClass());
-    List<League> leagues = repo.getLeagues();
+    List<Team> teamList = repo.getTeams();
     logger.info("About to load Team relationships using {} threads",numThreads);
     recordsWritten.set(0);
 
-    int subListSize = (int) Math.floor(leagues.size() / numThreads);
+    int subListSize = (int) Math.floor(teamList.size() / numThreads);
     long start = System.currentTimeMillis();
 
-    Lists.partition(leagues, subListSize).stream().parallel()
-        .forEach((leagueSubList) -> {
-          List<Team> teams = leagueSubList.stream()
-              .map(league -> repo.getTeams(league))
-              .flatMap(l -> l.orElse(Collections.emptyList()).stream()).
-                  collect(Collectors.toList());
+    Lists.partition(teamList, subListSize).stream().parallel()
+        .forEach((teamsSubList) -> {
 
-          doSubmitableWork(aeLogger, teams, MERGE_TEAM_RELATIONSHIPS);
+          submitableWorker.doSubmitableWork(aeLogger, teamsSubList, MERGE_TEAM_RELATIONSHIPS);
 
         });
     monitorThreadPool();
@@ -118,30 +106,6 @@ public class NFTeamLoader {
     }
   }
 
-  @Async("teamProcessorThreadPool")
-  private void doSubmitableWork(AggregateExceptionLogger aeLogger,
-                                List<Team> teams,
-                                String cypher) {
-    Neo4jTemplate
-        neo4jTemplate = getNeo4jTemplate();
 
-    logger.info("About to process {} teams ", teams.size());
-    Map<String, Object> map = new HashMap<String, Object>();
-    map.put("json", teams);
-    try {
-      new RetriableTask().retries(3).delay(200, TimeUnit.MILLISECONDS).step(500, TimeUnit.MILLISECONDS).execute(() -> {
-        neo4jTemplate.execute(cypher, map);
-        recordsWritten.addAndGet(teams.size());
-      });
-    } catch (Exception e) {
-      aeLogger
-          .error("Unable to update graph, teamCount={}", teams.size(),
-              e);
-    }
-  }
 
-  private Neo4jTemplate getNeo4jTemplate() {
-    Session session = sessionFactory.openSession();
-    return new Neo4jTemplate(session);
-  }
 }
---
+++ src/main/java/org/dentinger/tutorial/loader/nodefirst/TeamWorker.java
@@ -0,0 +1,60 @@
+package org.dentinger.tutorial.loader.nodefirst;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import org.dentinger.tutorial.domain.Team;
+import org.dentinger.tutorial.util.AggregateExceptionLogger;
+import org.dentinger.tutorial.util.RetriableTask;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.template.Neo4jTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeamWorker {
+
+  private static Logger logger = LoggerFactory.getLogger(NFTeamLoader.class);
+  private SessionFactory sessionFactory;
+  private final AtomicLong recordsWritten = new AtomicLong(0);
+
+  @Autowired
+  public TeamWorker(SessionFactory sessionFactory) {
+
+    this.sessionFactory = sessionFactory;
+  }
+
+  private Neo4jTemplate getNeo4jTemplate() {
+    Session session = sessionFactory.openSession();
+    return new Neo4jTemplate(session);
+  }
+
+  @Async("teamProcessorThreadPool")
+  public void doSubmitableWork(AggregateExceptionLogger aeLogger,
+                                List<Team> teams,
+                                String cypher) {
+    Neo4jTemplate
+        neo4jTemplate = getNeo4jTemplate();
+
+    logger.info("About to process {} teams ", teams.size());
+    Map<String, Object> map = new HashMap<String, Object>();
+    map.put("json", teams);
+    try {
+      new RetriableTask().retries(3).delay(200, TimeUnit.MILLISECONDS).step(500, TimeUnit.MILLISECONDS).execute(() -> {
+        neo4jTemplate.execute(cypher, map);
+        recordsWritten.addAndGet(teams.size());
+      });
+    } catch (Exception e) {
+      aeLogger
+          .error("Unable to update graph, teamCount={}", teams.size(),
+              e);
+    }
+  }
+
+}
